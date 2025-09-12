eb923bcf5312ae132210ecfaf22fcb3c6900653a
jsik94 <jsik94@vntgcorp.com>
2025-09-12 09:44:19 +0900

    chore(api-server): change test

--- apps/test-project/api-server/src/main.ts
+++ apps/test-project/api-server/src/main.ts
@@ -9,21 +9,19 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // CORS 활성화
   app.enableCors({
     origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 App is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
